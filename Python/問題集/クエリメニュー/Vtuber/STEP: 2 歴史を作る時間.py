6251fa120ae0bc82c03f5278956cd760d9d332f2
makimaki08 <08.kobayashi@gmail.com>
2023-07-21 22:01:57 +0900

    どうしても1問だけ解決しないが、解き方は合っているはず。

--- Python/問題集/クエリメニュー/Vtuber/STEP: 2 歴史を作る時間.py
+++ Python/問題集/クエリメニュー/Vtuber/STEP: 2 歴史を作る時間.py
@@ -1,3 +1,5 @@
+# 辞書型では、キーだけ取得「.keys()」、値だけ取得「.values()」、keyとvalueを同時に取得「.items()」がある。
+
 group_num, event_num = map(int,input().split())
 group_member = {input() for _ in range(group_num)}
 
@@ -6,8 +8,12 @@ for i in range(event_num):
 	year, name = input().split()
 	historys[int(year)] = name
 
-# print(historys)
-# print(historys[593])
-# print(sorted(historys))
-for history_num in sorted(historys):
-	print(historys[history_num])
+# print(historys.keys()) # dict_keys([645, 593, 2058, 29484, 374759])
+# print(historys.values()) # dict_values(['nao', 'hiro', 'yuki', 'nao', 'nao'])
+# print(historys.items()) # dict_items([(645, 'nao'), (593, 'hiro'), (2058, 'yuki'), (29484, 'nao'), (374759, 'nao')])
+
+
+for year, name in sorted(historys.items()):
+	print(name)
+
+# 参考： https://magazine.techacademy.jp/magazine/19309
