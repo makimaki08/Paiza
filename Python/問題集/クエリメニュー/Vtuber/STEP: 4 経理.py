1973a656acbfa42f5ca487d2f21ec6563c9c23ba
makimaki08 <08.kobayashi@gmail.com>
2023-07-22 08:00:59 +0900

    STEP4自力突破 連想配列にタプル型の配列を設定させることで、対応できると知った

--- Python/問題集/クエリメニュー/Vtuber/STEP: 4 経理.py
+++ Python/問題集/クエリメニュー/Vtuber/STEP: 4 経理.py
@@ -1,19 +1,17 @@
 N, K = map(int, input().split())
-departments = [input() for _ in range(N)]
+departments = {input(): [] for _ in range(N)}
 # print(departments)
 
-receipts = {}
 for i in range(K):
 	department_name, order_id, fee = input().split()
-	receipts[order_id] = (department_name, int(fee))
-	# print(department_name, order_id, fee)
+	departments[department_name].append([order_id, int(fee)])
 
-# print(receipts)
-# print(receipts.items())
+print(departments)
+print(departments.items())
 
-for department in departments:
-	print(department)
-	for order_id, receipt_arr in receipts.items():
-		if(department == receipt_arr[0]):
-			print(order_id, receipt_arr[1])
-	print("-----")
+# for department in departments:
+# 	print(department)
+# 	for order_id, receipt_arr in departments.items():
+# 		if(department == receipt_arr[0]):
+# 			print(order_id, receipt_arr[1])
+# 	print("-----")
