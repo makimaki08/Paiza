a924ff795bb838f77ec8c7d65d29de5ca61e0b7f
makimaki08 <08.kobayashi@gmail.com>
2023-07-22 15:06:44 +0900

    自力突破（理論はよくわかっていない、特にタプル）

--- Python/問題集/クエリメニュー/Vtuber/FINAL問題 Vtuber.py
+++ Python/問題集/クエリメニュー/Vtuber/FINAL問題 Vtuber.py
@@ -5,17 +5,24 @@ memberships = []
 for i in range(N):
 	input_arr = input().split()
 	name = input_arr[0]
-	act = input_arr[1]
+	verb = input_arr[1]
 
-	if act == "give":
-		superchats[int(input_arr[2])] = name
+	if verb == "give":
+		money = int(input_arr[2])
+		# 過去にスパチャをしていた場合
+		if name in superchats:
+			superchats[name] = (superchats[name][0]+money, name)
 
-	elif act == "join":
+		# 過去にスパチャをしておらず、一見さんの場合
+		else:
+			superchats[name] = (money, name)
+
+	else:
 		memberships.append(name)
-
+# print(superchats["yoyo"])
 # print(sorted(superchats, reverse=False))
 # print(superchats.items())
-for money, name in sorted(superchats.items(), reverse = True):
+for name, money in sorted(superchats.items(), key=lambda x:x[1], reverse = True):
 	print(name)
 
 for member in sorted(memberships):
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Paiza用のリポジトリ
 Paizaが回答を公開している事もあり、PrivateからPublicに変更
+
+
+Pythonテクニック
+	辞書型
+		キーだけ取得「.keys()」
+		値だけ取得「.values()」
+		keyとvalueを同時に取得「.items()」
+		参考資料
+			https://qiita.com/ysuzuki19/items/ec592d30566a436996aa
