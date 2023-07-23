51e5de9850c75134e6609d2caebb9b51290c5e47
makimaki08 <08.kobayashi@gmail.com>
2023-07-23 09:49:45 +0900

    STEP3自力突破

--- Python/問題集/クエリメニュー/平方分割/STEP: 3 二次元累積和.py
+++ Python/問題集/クエリメニュー/平方分割/STEP: 3 二次元累積和.py
@@ -1,32 +1,8 @@
 import numpy as np
 H, W, N = map(int, input().split())
-
-box = []
-for h in range(H):
-	input_arr = list(map(int, input().split()))
-	input_arr.insert(0,0)
-	box.append(input_arr)
-# print(box)
+box = [list(map(int, input().split())) for _ in range(H)]
+box = np.pad(box, ((1, 0), (1, 0)))
 
 for n in range(N):
-	sum_value = 0
-	max_y, max_x = map(int, input().split())
-	for y in range(max_y):
-		sum_arr = np.cumsum(box[y])
-		# tmp = sum_arr[max_x]- sum_arr[0]
-		# print(sum_arr)
-		# print(sum_arr[max_x], sum_arr[0])
-		# print(tmp)
-
-		sum_value += sum_arr[max_x]
-	print(sum_value)
-	# print("--------------")
-
-# arr = np.zeros((H,W))
-# print(arr)
-# box = [ list(map(int,input().split(" "))) for i in range(H)]
-# print(box[:1][:1])
-
-# for n in range(N):
-# 	x, y = map(int, input().split())
-# 	
+	y, x = map(int, input().split())
+	print(np.sum(box[:y+1,:x+1]))
