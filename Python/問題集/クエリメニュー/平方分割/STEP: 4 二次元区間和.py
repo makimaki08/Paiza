cce0b52aa8548b4385be5192577493ad3c302145
makimaki08 <08.kobayashi@gmail.com>
2023-07-24 20:28:56 +0900

    STEP4クリア

---
+++ Python/問題集/クエリメニュー/平方分割/STEP: 4 二次元区間和.py
@@ -0,0 +1,11 @@
+import numpy as np
+H, W, N = map(int, input().split())
+box = np.array([list(map(int, input().split())) for _ in range(H)])
+box = np.pad(box, ((1, 0), (1, 0)))
+
+for _ in range(N):
+	min_y, min_x, max_y, max_x = map(int, input().split())
+	max_y+=1
+	max_x+=1
+	print(np.sum(box[min_y:max_y,min_x:max_x]))
+
