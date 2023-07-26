c5cd7d8bcc34102cd9d0d727534861bb196dc698
makimaki08 <08.kobayashi@gmail.com>
2023-07-26 19:38:38 +0900

    STEP2を、「Inner Circle - Outer Circle」で求めるように変更し突破

--- Python/問題集/クエリメニュー/点の幅/STEP: 2 ドーナツ.py
+++ Python/問題集/クエリメニュー/点の幅/STEP: 2 ドーナツ.py
@@ -1,9 +1,21 @@
 import numpy as np
+def calc_circle(y,x,value):
+	top = y - value
+	bottom = y + value
+	left = x - value
+	right = x + value
+	return top,bottom,left,right
+
+
 H, W, N = map(int, input().split())
 box = [list(map(int, input().split())) for _ in range(H)]
-print(box)
-np.pad(box, ((1, 0), (1, 0)))
+box = np.pad(box, ((1, 0), (1, 0)))
+# print(box)
 
 for n in range(N):
 	y, x, b, s = map(int, input().split())
-	print(y, x, b, s)
+	in_top, in_bottom, in_left, in_right = calc_circle(y,x,b//2)
+	out_top, out_bottom, out_left, out_right = calc_circle(y,x,s//2)
+	in_circle_sum = np.sum(box[in_top:in_bottom+1,in_left:in_right+1])
+	out_circle_sum = np.sum(box[out_top:out_bottom+1,out_left:out_right+1])
+	print(in_circle_sum - out_circle_sum)
