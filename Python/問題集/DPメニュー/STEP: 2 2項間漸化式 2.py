dbbdd1335c3f77c1929af17fd00eb85a9ab9acb2
makimaki08 <08.kobayashi@gmail.com>
2023-07-28 07:59:30 +0900

    STEP2突破 任意の値で初期化するより、あらかじめきまった値で初期化する方が早い ※あらかじめ1000がMAXと決まっていたため、rangeを考えて1000+1で初期化

--- Python/問題集/DPメニュー/STEP: 2 2項間漸化式 2.py
+++ Python/問題集/DPメニュー/STEP: 2 2項間漸化式 2.py
@@ -1,10 +1,10 @@
 x, d = map(int, input().split())
-q = int(input())
-arr = [x] * (q+1)
+arr = [x] * (1000+1)
 
-for i in range(2,q+1):
+for i in range(2,1000+1):
 	arr[i] = arr[i-1] + d
 
+q = int(input())
 for _ in range(q):
 	index = int(input())
 	print(arr[index])
