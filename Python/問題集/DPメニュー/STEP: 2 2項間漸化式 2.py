70cdeecd4f2aff498aa22fd525af61cc20f4020a
makimaki08 <08.kobayashi@gmail.com>
2023-07-28 07:57:37 +0900

    ランタイムエラーで失敗になる。。。 あらかじめ、1000で初期化した方が早い？

--- Python/問題集/DPメニュー/STEP: 2 2項間漸化式 2.py
+++ Python/問題集/DPメニュー/STEP: 2 2項間漸化式 2.py
@@ -1,1 +1,10 @@
-STEP: 2 2項間漸化式 2
+x, d = map(int, input().split())
+q = int(input())
+arr = [x] * (q+1)
+
+for i in range(2,q+1):
+	arr[i] = arr[i-1] + d
+
+for _ in range(q):
+	index = int(input())
+	print(arr[index])
