b88cfcc3e5242259e272112795c9d50e908f4ae4
makimaki08 <08.kobayashi@gmail.com>
2023-07-28 07:09:06 +0900

    STEP1突破 等差数列として、xで配列を初期化する考えがなかった

--- Python/問題集/DPメニュー/STEP: 1 2項間漸化式 1.py
+++ Python/問題集/DPメニュー/STEP: 1 2項間漸化式 1.py
@@ -1,9 +1,8 @@
 import numpy as np
 x, d, k = map(int, input().split())
-arr = np.zeros(k+1)
-arr[0] = x
+arr = [x] * (k+1)
 
-for i in range(1,k+1):
+for i in range(2, k+1):
 	arr[i] = arr[i-1] + d
 
 print(int(arr[k]))
