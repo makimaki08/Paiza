f3d0ee192abe06ec738c6047474d7601dae12fe7
makimaki08 <08.kobayashi@gmail.com>
2023-08-01 06:56:37 +0900

    STEP4の自己回答を追加

--- Python/問題集/DPメニュー/STEP: 4 特殊な2項間漸化式 2.py
+++ Python/問題集/DPメニュー/STEP: 4 特殊な2項間漸化式 2.py
@@ -1,1 +1,16 @@
-STEP: 4 特殊な2項間漸化式 2
+import numpy as np
+x, d_1, d_2 = map(int, input().split())
+Q = int(input())
+
+arr = np.zeros(1001)
+arr[1] = x
+for i in range(2,1001):
+	if i%2==1:
+		arr[i] = arr[i-1] + d_1
+	else:
+		arr[i] = arr[i-1] + d_2
+
+
+for _ in range(Q):
+	index = int(input())
+	print(int(arr[index]))
