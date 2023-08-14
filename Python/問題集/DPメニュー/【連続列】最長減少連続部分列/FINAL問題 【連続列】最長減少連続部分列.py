42035400f5953af3dd332e2d13e3095151d8587a
makimaki08 <08.kobayashi@gmail.com>
2023-08-14 20:13:58 +0900

    【連続列】最長減少連続部分列クリア

--- Python/問題集/DPメニュー/【連続列】最長減少連続部分列/FINAL問題 【連続列】最長減少連続部分列.py
+++ Python/問題集/DPメニュー/【連続列】最長減少連続部分列/FINAL問題 【連続列】最長減少連続部分列.py
@@ -1,1 +1,13 @@
-import nu
+import numpy as np
+
+N = int(input())
+humans = [int(input()) for _ in range(N)]
+ans = np.ones(N)
+
+# print(humans)
+# print(ans)
+
+for i in range(1,N):
+	ans[i] = ans[i-1]+1 if humans[i-1]>=humans[i] else 1
+
+print(int(max(ans)))
