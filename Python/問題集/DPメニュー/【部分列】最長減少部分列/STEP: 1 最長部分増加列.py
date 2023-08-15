51e2bb51e1a5d89497860777fa63504bcc4eedfc
makimaki08 <08.kobayashi@gmail.com>
2023-08-15 20:42:17 +0900

    一部の回答は突破できるが、他の回答でつまづく。 つまづき理由は不明

--- Python/問題集/DPメニュー/【部分列】最長減少部分列/STEP: 1 最長部分増加列.py
+++ Python/問題集/DPメニュー/【部分列】最長減少部分列/STEP: 1 最長部分増加列.py
@@ -2,27 +2,25 @@ import numpy as np
 
 N = int(input())
 arr = [int(input()) for _ in range(N)]
-ans = np.zeros((N+1,N))
-
+ans = np.zeros(N)
+ans[0] = 1
 
 for i in range(1,N):
-	ans[i][0] = 1
-	for j in range(1,N):
-		if arr[j-1]<=arr[j]:
-			ans[i][j] = max(1, ans[i][j-1]+1)
-		else:
-			ans[i][j] = ans[i][j-1]
+	if arr[i-1]<=arr[i]:
+		ans[i] = ans[i-1] + 1
+	else:
+		ans[i] = ans[i-1]
 
-# print(int(max(ans)))
-print(ans)
+# print(ans)
+print(int(ans[N-1]))
 
 """
 5
-100
-102
-101
-91
-199
+100　→ 1
+102　→ 2
+101　→ 2
+91　→ 2
+199　→ 3
 
 3
 """
